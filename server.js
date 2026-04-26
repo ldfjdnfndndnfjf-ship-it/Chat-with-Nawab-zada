1245befeb46f491d18c44be6964c432e8f2d935c
Nawab512 <ldfjdnfndndnfjf@gmail.com>
2026-04-26 08:12:28 +0500

    Update server.js

--- server.js
+++ server.js
@@ -6,11 +6,14 @@ const io = require('socket.io')(http);
 app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
 
 io.on('connection', (socket) => {
-  const ip = socket.handshake.address; // IP capture
-  socket.on('chat message', (data) => {
-    // Admin ko IP aur message dono show honge
-    io.emit('chat message', { name: data.name, msg: data.msg, ip: ip });
-  });
+    // Ye wala naya update hai jani
+    socket.on('chat message', (data) => {
+        io.emit('chat message', { 
+            name: data.name, 
+            msg: data.msg, 
+            ip: socket.handshake.address 
+        });
+    });
 });
 
 http.listen(3000, () => console.log('Nawab ZADA Server Running!'));
