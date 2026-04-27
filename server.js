fb3a99a0e87fbabba019dc39d72fb136e07a55ec
Nawab512 <ldfjdnfndndnfjf@gmail.com>
2026-04-27 06:59:22 +0500

    Update server.js

--- server.js
+++ server.js
@@ -3,17 +3,33 @@ const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+// Blacklist array (yahan block kiye gaye IP save honge)
+let blockedIPs = [];
 
 io.on('connection', (socket) => {
-    // Ye wala naya update hai jani
+    // Real IP header (Vercel ke liye)
+    const ip = socket.handshake.headers['x-forwarded-for'] || socket.handshake.address;
+
+    // Agar IP block list mein hai to connection reject
+    if (blockedIPs.includes(ip)) {
+        socket.disconnect();
+        return;
+    }
+
     socket.on('chat message', (data) => {
-        io.emit('chat message', { 
-            name: data.name, 
-            msg: data.msg, 
-            ip: socket.handshake.address 
-        });
+        // Auto-Remove Spam/Links
+        if (data.msg.includes("http") || data.msg.length > 200) {
+            console.log("Spam detected from: " + ip);
+            return; // Msg process nahi hoga
+        }
+
+        io.emit('chat message', { name: data.name, msg: data.msg, ip: ip });
+    });
+
+    // Block logic (Admin se command aayi to)
+    socket.on('block-ip', (ipToBlock) => {
+        blockedIPs.push(ipToBlock);
     });
 });
 
-http.listen(3000, () => console.log('Nawab ZADA Server Running!'));
+http.listen(3000);
